f1cdf676afd5d6d911a06981be2a9b217357db64
TayeburRahman <tayebrayhan101@gmail.com>
2022-05-28 04:39:45 +0600

    update

--- src/local/DashbordHome/serviceCompo.js
+++ src/local/DashbordHome/serviceCompo.js
@@ -1,10 +1,10 @@
-import { CardActionArea } from "@mui/material";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import img from "../../image/featuredbannerimage.png";
 
-const DashboardHome = () => {
+const ServiceCompo = () => {
   return (
     <div className="container mt-5 pt-3 pb-5">
       <Typography sx={{ letterSpacing: 6, textAlign: "center"}}  className="mb-1 mt-4" className="dashboardTColor" >THE PRODUCTS</Typography>
@@ -15,9 +15,9 @@ const DashboardHome = () => {
         <Grid container spacing={0} >
           <Grid item xs={12} md={4}>
             <div className="p-3">
-              <CardActionArea className="d-grid p-3" style={{justifyItems: 'center'}}>
+              <Box className="d-grid p-3" style={{justifyItems: 'center'}}>
                 <i class="fas fa-running cart-icon"></i>
-                <div className="d-grid" style={{justifyItems: 'center'}}>
+                <div className="d-grid" style={{justifyItems: 'center', textAlign: 'center'}}>
                   <Typography gutterBottom variant="h5" component="div">
                     Running
                   </Typography>
@@ -25,30 +25,43 @@ const DashboardHome = () => {
                     Lizards are a widespread group of squamate reptiles, with
                   </Typography>
                 </div>
-              </CardActionArea>
+              </Box>
             </div>
             <div className="p-3">
-              <CardActionArea className="d-grid p-3" style={{justifyItems: 'center'}}>
-                <i class="fas fa-microphone cart-icon"></i>
-                <div className="d-grid" style={{justifyItems: 'center'}}>
+              <Box className="d-grid p-3" style={{justifyItems: 'center'}}> 
+                <div className="d-grid" style={{justifyItems: 'center', textAlign: 'center'}}>
+                <img className='' src="https://i.ibb.co/fDWY6fr/icon-128x128-120x.png" width='20%' alt="BigCo Inc. logo"/>
                   <Typography gutterBottom variant="h5" component="div">
-                    Microphone
+                   30 DAYS RETURN
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    ranging across all continents except Antarctica
+                   Simply return it within 30 for an exchange. Contact us 200 hours.
                   </Typography>
                 </div>
-              </CardActionArea>
+              </Box>
             </div>
           </Grid>
           <Grid className="d-flex" item xs={12} md={4} style={{justifyContent: 'center'}} >
             <img src={img} alt="" />
           </Grid>
           <Grid item xs={12} md={4}>
+          <div className="p-3">
+              <Box className="d-grid p-3" style={{justifyItems: 'center'}}>
+                 <div className="d-grid" style={{justifyItems: 'center', textAlign: 'center'}}>
+                 <img className='' src="https://i.ibb.co/YhPXwRW/free-delivery-64-px-2x-120x.png" width='20%' alt="BigCo Inc. logo"/>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Free Shipping
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                  Free shipping on all US order or order above $100        
+                  </Typography>
+                </div>
+              </Box>
+            </div>
             <div className="p-3">
-              <CardActionArea className="d-grid p-3" style={{justifyItems: 'center'}}>
+              <Box className="d-grid p-3" style={{justifyItems: 'center'}}>
                 <i class="fas fa-street-view cart-icon"></i>
-                <div className="d-grid" style={{justifyItems: 'center'}}>
+                <div className="d-grid" style={{justifyItems: 'center', textAlign: 'center'}}>
                   <Typography gutterBottom variant="h5" component="div">
                     Location
                   </Typography>
@@ -57,20 +70,7 @@ const DashboardHome = () => {
                     species
                   </Typography>
                 </div>
-              </CardActionArea>
-            </div>
-            <div className="p-3">
-              <CardActionArea className="d-grid p-3" style={{justifyItems: 'center'}}>
-                <i class="fas fa-thermometer-quarter cart-icon"></i>
-                <div className="d-grid" style={{justifyItems: 'center'}}>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles
-                  </Typography>
-                </div>
-              </CardActionArea>
+              </Box>
             </div>
           </Grid>
         </Grid>
@@ -79,4 +79,4 @@ const DashboardHome = () => {
   );
 };
 
-export default DashboardHome;
+export default ServiceCompo;
